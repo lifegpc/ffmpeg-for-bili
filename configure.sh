daac397f6bb132917d28ecc2bfb520d71ba2cf57
lifegpc <g1710431395@gmail.com>
2021-08-15 10:41:33 +0800

    update configure

--- configure.sh
+++ configure.sh
@@ -7,13 +7,18 @@ cd ffmpeg
     --disable-ffplay \
     --disable-ffprobe \
     --disable-doc \
-    --disable-everything \
+    --disable-decoders \
     --enable-decoder=ass,bmp,jpeg2000,jpegls,mjpeg,mjpegb,png,srt,ssa,webp,webvtt \
+    --disable-encoders \
     --enable-encoder=mjpeg,movtext \
+    --disable-hwaccels \
+    --disable-demuxers \
     --enable-demuxer=ass,flac,hls,image_bmp_pipe,image_jpeg_pipe,image_jpegls_pipe,image_png_pipe,image_webp_pipe,matroska,mjpeg,mjepg_2000,mov,srt,webvtt \
+    --disable-muxers \
     --enable-muxer=matroska,mov \
+    --disable-parsers \
     --enable-parser=bmp,flac,h264,hevc,jpeg2000,mjpeg,png,webp \
-    --enable-protocol=concat,concatf,file,ftp,hls,http,https,pipe,tls \
+    --disable-filters \
     --enable-gmp \
     --enable-gnutls \
     --pkg-config-flags=--static \
