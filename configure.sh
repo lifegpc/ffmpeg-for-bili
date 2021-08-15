34bfe1fa21c0518edc115bbec2d90dc06efc1321
lifegpc <g1710431395@gmail.com>
2021-08-15 11:19:51 +0800

    add ffmetadata demuxer

--- configure.sh
+++ configure.sh
@@ -13,7 +13,7 @@ cd ffmpeg
     --enable-encoder=mjpeg,movtext \
     --disable-hwaccels \
     --disable-demuxers \
-    --enable-demuxer=ass,flac,hls,image_bmp_pipe,image_jpeg_pipe,image_jpegls_pipe,image_png_pipe,image_webp_pipe,matroska,mjpeg,mjepg_2000,mov,mpegts,srt,webvtt \
+    --enable-demuxer=ass,ffmetadata,flac,hls,image_bmp_pipe,image_jpeg_pipe,image_jpegls_pipe,image_png_pipe,image_webp_pipe,matroska,mjpeg,mjepg_2000,mov,mpegts,srt,webvtt \
     --disable-muxers \
     --enable-muxer=matroska,mov \
     --disable-parsers \
